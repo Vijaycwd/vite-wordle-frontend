64b1e5761567ce9891a7d0dc2ec90c11d956b1ef
Vijaycwd <vijisubramani25@gmail.com>
2024-08-07 11:37:06 +0530

    Add Header bar in userlogin.jsx

--- src/auth/Userlogin.jsx
+++ src/auth/Userlogin.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Loginform from '../components/Loginform';
+import Headerbar from '../components/Headerbar'
 function Userlogin() {
    
   return (
     <>
+    <Headerbar/>
     <Loginform/>
     </>
   )
