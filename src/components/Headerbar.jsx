42bfd86aba478c15f47d6213e936de2bb3bdde58
Vijaycwd <vijisubramani25@gmail.com>
2024-08-07 16:38:38 +0530

    Update Header.jsx

--- src/components/Headerbar.jsx
+++ src/components/Headerbar.jsx
@@ -30,50 +30,49 @@ const Headerbar = () => {
   }
   return (
     <Container>
-      <Row className="d-flex align-items-center">
-        <Col md={2}><img className='img-fluid' src={Logo} style={{width: 150 + 'px'}} alt = "logo"></img></Col>
-        <Col md={8}><h1 className='text-center'>WordGAMLE</h1></Col>
-        <Col md={2} className="d-flex">
-        
-        <Link to="/statistics">
-          <svg xmlns="http://www.w3.org/2000/svg" className="m-2 bi bi-bar-chart-fill" width="25" height="25" fill="currentColor"  viewBox="0 0 16 16">
-              <path d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z"/>
-          </svg> 
-        </Link>
-        <div ref={ref}>
-          <Link onClick={handleClick}><svg xmlns="http://www.w3.org/2000/svg" className="m-2 bi bi-people-fill"  width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
-        </svg></Link>
+      <Row>
+        <Col sm><img className='img-fluid' src={Logo} style={{width: 150 + 'px'}} alt = "logo"></img></Col>
+        <Col sm><h1 className='text-center'>WordGAMLE</h1></Col>
+        <Col sm>
+          <Link to="/statistics">
+            <svg xmlns="http://www.w3.org/2000/svg" className="m-2 bi bi-bar-chart-fill" width="25" height="25" fill="currentColor"  viewBox="0 0 16 16">
+                <path d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z"/>
+            </svg> 
+          </Link>
+          <div ref={ref}>
+            <Link onClick={handleClick}><svg xmlns="http://www.w3.org/2000/svg" className="m-2 bi bi-people-fill"  width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+          </svg></Link>
 
-          <Overlay
-            show={show}
-            target={target}
-            placement="bottom"
-            container={ref}
-            containerPadding={20}
-          >
-            <Popover id="popover-contained">
-              {/* <Popover.Header as="h3">User </Popover.Header> */}
-              <Popover.Body>
-                <div className='text-center'>
-                  {userData ? (
-                    <div>
-                      <img src={`https://wordle-server-gf3r.onrender.com/public/uploads/${userData.avatar}`} alt="User Avatar" />
-                      <p className='fs-4 m-0'>{userData.username}</p>
-                      <p>{userData.email}</p>
-                      <Button onClick ={logout}>Logout</Button>
-                    </div>
-                ) : (
-                  <Button onClick ={login}>Logout</Button>
-                )}
-                
-                </div> 
-              </Popover.Body>
-            </Popover>
-          </Overlay>
-        </div>
+            <Overlay
+              show={show}
+              target={target}
+              placement="bottom"
+              container={ref}
+              containerPadding={20}
+            >
+              <Popover id="popover-contained">
+                {/* <Popover.Header as="h3">User </Popover.Header> */}
+                <Popover.Body>
+                  <div className='text-center'>
+                    {userData ? (
+                      <div>
+                        <img src={`https://wordle-server-gf3r.onrender.com/public/uploads/${userData.avatar}`} alt="User Avatar" />
+                        <p className='fs-4 m-0'>{userData.username}</p>
+                        <p>{userData.email}</p>
+                        <Button onClick ={logout}>Logout</Button>
+                      </div>
+                  ) : (
+                    <Button onClick ={login}>Logout</Button>
+                  )}
+                  
+                  </div> 
+                </Popover.Body>
+              </Popover>
+            </Overlay>
+          </div>
         </Col>
-      </Row>    
+      </Row>  
     </Container>
   );
 }
