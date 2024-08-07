1cf975f7d8605809c439fe100a848535a36ef1b7
Vijaycwd <vijisubramani25@gmail.com>
2024-08-07 17:00:08 +0530

    Update header

--- src/components/Headerbar.jsx
+++ src/components/Headerbar.jsx
@@ -37,7 +37,7 @@ const Headerbar = () => {
         <Col xs={6}>
           <h2 className='text-center'>WordGAMLE</h2>
         </Col>
-        <Col xs={3} className="d-flex">
+        <Col xs={3} className="d-flex justify-content-end">
         <Link to="/statistics">
             <svg xmlns="http://www.w3.org/2000/svg" className="m-2 bi bi-bar-chart-fill" width="25" height="25" fill="currentColor"  viewBox="0 0 16 16">
                 <path d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z"/>
