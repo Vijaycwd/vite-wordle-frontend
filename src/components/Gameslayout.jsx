3927f22919cccbaea5fd2cdda694f8a020c8ef52
Vijaycwd <vijisubramani25@gmail.com>
2024-09-14 17:51:56 +0530

    Update all

--- src/components/Gameslayout.jsx
+++ src/components/Gameslayout.jsx
@@ -1,37 +1,37 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap'
 import Wordlegamesection from './Games/Wordle/Wordlegamesection';
-import { UserContext } from '../constant/UserContext'; 
 
 function Gameslayout() {
+  const userAuthData = JSON.parse(localStorage.getItem('auth'));
+  const userData = userAuthData;
+    
+    // console.log('User Data State:', userData.username);
 
-  const { userDatadetails } = useContext(UserContext);
-  console.log(userDatadetails);
-
-  return (
-    <Container>
-      <Row className="justify-content-center align-items-center">
-        <Col className="text-center py-3">
-          <h2>{"Welcome " + userData.username + "!"}</h2>
-        </Col>
-      </Row>
-      <Row className="justify-content-center align-items-center">
-        <Wordlegamesection />
-      </Row>
-      <Row className="justify-content-center align-items-center">
-        <Col md={6} className="py-5">
-          <div>
-            <p>Click the “Play” button to go to the Wordle website and play. Then:</p>
-            <ol>
-              <li><strong>PLAY:</strong> Play Wordle</li>
-              <li><strong>COPY:</strong> Click SHARE, then COPY to copy your Wordle result</li>
-              <li><strong>PASTE:</strong> Navigate back to WordGAMLE.com to paste your Wordle result</li>
-            </ol>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container>
+           <Row className="justify-content-center align-items-center">
+                <Col  className='text-center py-3'>
+                    <h2>{"Welcome "+userData.username+"!"}</h2>
+                </Col>
+           </Row>
+           <Row className="justify-content-center align-items-center">
+            <Wordlegamesection/>  
+            </Row>
+            <Row className="justify-content-center align-items-center">
+              <Col md={6} className='py-5'>
+                <div>
+                  <p>Click the “Play” button to go to the Wordle website and play. Then:</p>
+                  <ol>
+                    <li><strong>PLAY:</strong> Play Wordle</li>
+                    <li><strong>COPY:</strong> Click SHARE, then COPY to copy your Wordle result</li>
+                    <li><strong>PASTE:</strong> Navigate back to WordGAMLE.com to paste your Wordle result</li>
+                  </ol>
+                </div>
+              </Col>
+            </Row>
+        </Container>
+    )
 }
 
-export default Gameslayout;
+export default Gameslayout 
--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import Gameslayout from '../components/Gameslayout';
-import { UserProvider } from '../constant/UserContext';
 function Statistics() {
   return (
     <>
-      <UserProvider>
-        <Gameslayout/>
-      </UserProvider>
+      <Gameslayout/>
     </>
     
   )
