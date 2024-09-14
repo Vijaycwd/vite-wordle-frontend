888513d818f537a7b641efd95d17e7b09b589706
Vijaycwd <vijisubramani25@gmail.com>
2024-09-14 17:55:40 +0530

    Update Headerbar.jsx

--- src/components/Header/Headerbar.jsx
+++ src/components/Header/Headerbar.jsx
@@ -1,13 +1,8 @@
-import React, { useState, useRef,  useEffect } from 'react';
-import { Container, Row, Col, Button, Overlay, Popover, Form } from 'react-bootstrap';
+import React, { useState, useRef } from 'react';
+import { Container, Row, Col, Button, Overlay, Popover } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Logo from '../../Logo.png';
 import TitleLogo from '../../WordleTitleLogo.png'
-import Registerform from '../../auth/Registerform';
-import { useNavigate } from "react-router-dom";
-import Axios from "axios";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const Headerbar = () => {
   const USER_AUTH_DATA = JSON.parse(localStorage.getItem('auth'));
@@ -15,20 +10,6 @@ const Headerbar = () => {
   const [show, setShow] = useState(false);
   const [target, setTarget] = useState(null);
   const ref = useRef(null);
-
-
-  const [userid, setUserid] = useState();
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [confirmpassword, setConfirmpassword] = useState();
-  const [avatar, setAvatar] = useState();
-  const [errors, setErrors] = useState({});
-  const navigate = useNavigate();
-
-  const handleUpload = async (e) => {
-    setAvatar(e.target.files[0]);
-  }
 
   const handleClick = (event) => {
     setShow(!show);
@@ -44,66 +25,8 @@ const Headerbar = () => {
     event.preventDefault();
     window.location.href = '/';
   };
-  const [isPopupVisible, setPopupVisible] = useState(false);
-  const handleOpenPopup = (_id, username, email, password) => {
-    setPopupVisible(true);
-    setUsername(username);
-    setEmail(email);
-    setPassword(password);
-    setConfirmpassword(password);
-    setAvatar(avatar);
-    setUserid(_id);
-  };
-  const updateUser = async (event) => {
-    setPopupVisible(false);
-    const userObject = {
-        username: username,
-        email: email,
-        password: password,
-        confirmpassword: confirmpassword,
-        avatar: avatar
-    }
-    const validation =(userObject) =>{
-        const errors = {};
 
-        if(!userObject.username){   
-            errors.username = "Username Required";
-        }
-        if(!userObject.email){   
-            errors.email = "Email Required";
-        }
-        // if(!userObject.password){   
-        //     errors.password = "Password Required";
-        // }
-        // if(!userObject.confirmpassword){   
-        //     errors.confirmpassword = "Password Required";
-        // }
-        return errors;
-    }
-    setErrors(validation(userObject));
-    console.log(userObject);
-    try {
-        const response = await Axios.put(`https://wordle-server-nta6.onrender.com/use/${userid}`, userObject, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        });
-        if (response.status === 200) {
-            toast.success(response.data.message, {
-              position: "top-center"
-            });
-            localStorage.setItem('auth', JSON.stringify(response.data.user));
-        }
-    } catch (error) {
-        console.error("Error updating user:", error.response ? error.response.data : error.message);
-        toast.error(error.response.data.message, {
-            position: "top-center"
-        });
-        console.log(error.response.data.message);
-    }
-  } 
   return (
-    <>
     <Container>
       <Row className="justify-content-center align-items-center py-2">
         <Col xs={3}>
@@ -139,15 +62,15 @@ const Headerbar = () => {
                     {!userData || Object.keys(userData).length <= 0 ? (
                       <Button onClick={login}>Login</Button>
                     ) : (
+                      <>
                       <div>
                         <img src={`https://wordle-server-nta6.onrender.com/public/uploads/${userData.avatar}`} alt="User Avatar" className="img-fluid" />
                         <p className='fs-4 m-0'>{userData.username}</p>
                         <p>{userData.email}</p>
-                        <div className='user-button'>
-                          <Button onClick={()=>handleOpenPopup(userData._id, userData.username, userData.email)}>Profile</Button>
-                          <Button onClick={logout}>Logout</Button>
-                        </div>
+                        <Button onClick={logout}>Logout</Button>
+                        <Button onClick={() => editUser(userData.username, userData.email, userData._id, true)}>Edit</Button>
                       </div>
+                      </>
                     )}
                   </div>
                 </Popover.Body>
@@ -156,51 +79,7 @@ const Headerbar = () => {
           </div>
         </Col>
       </Row>
-      {isPopupVisible && (
-        <div className="user-profile-popup">
-            <Container>
-                <Row className='align-content-center justify-content-center'> 
-                    <Col md={4} className="user-register-form">
-                        <img src={Logo} alt="logo" className='d-block m-auto'></img>
-                        <h5>Create New Account</h5>
-                        <Form className="js-validation-signup ">
-                            <Form.Group>
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control type="text" className="" value={username} onChange={(e) => { setUsername(e.target.value);}} placeholder='Enter the name'/>
-                                {errors.username && <p className='form-validation-error'>{errors.username}</p>}
-                            </Form.Group>
-                            
-                            <Form.Group>
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email"  value={email} onChange={(e) => { setEmail(e.target.value);}} placeholder='Enter the email'/>
-                                {errors.email && <p className='form-validation-error'>{errors.email}</p>}
-                            </Form.Group>
-
-                            <Form.Group >
-                                <Form.Label>New Password</Form.Label>
-                                <Form.Control type="password" className=""  onChange={(e) => { setPassword(e.target.value);}} placeholder='Enter the password'/>
-                                {errors.password && <p className='form-validation-error'>{errors.password}</p>}
-                            </Form.Group>
-                            <Form.Group >
-                                <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control type="password" className=""  onChange={(e) => { setConfirmpassword(e.target.value);}} placeholder='Enter the conform password'/>
-                                {errors.confirmpassword && <p className='form-validation-error'>{errors.confirmpassword}</p>}
-                            </Form.Group>
-                            <Form.Group controlId="formFile" className="mb-3">
-                                <Form.Label>Profile Picture</Form.Label>
-                                <Form.Control type="file" name="avatar" onChange={handleUpload}  />
-                            </Form.Group>
-                            <Button className="btn btn-block btn-hero-lg btn-hero-success mt-4"  onClick={() => updateUser()} ><i className="fa fa-fw fa-plus mr-1"></i>Update</Button>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-      )}
-      
     </Container>
-    <ToastContainer />
-    </>
   );
 };
 
