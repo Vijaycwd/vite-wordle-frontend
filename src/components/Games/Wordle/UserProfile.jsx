867a7fe29704b564de37d6cd14b5547f6386ab71
Vijaycwd <vijisubramani25@gmail.com>
2024-09-15 22:19:09 +0530

    Update UserProfile.jsx

--- src/components/Games/Wordle/UserProfile.jsx
+++ src/components/Games/Wordle/UserProfile.jsx
@@ -9,8 +9,10 @@ import { useLocation } from 'react-router-dom';
 
 function UserProfile() {
     const location = useLocation();
-    const { username, email, id, isEditing } = location.state || {}; 
-    // const [username, setUsername] = useState();
+    // const { username, email, id, isEditing } = location.state || {}; 
+    const [username, setUsername] = useState(location.state?.username || "");
+    const [email, setEmail] = useState(location.state?.email || "");
+    const id = location.state?.id;
     // const [email, setEmail] = useState();
     
     const [password, setPassword] = useState(null);
