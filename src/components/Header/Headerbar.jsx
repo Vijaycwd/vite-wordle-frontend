9ac00eccc181330368bf3e19eabe361bd5551dd5
Vijaycwd <vijisubramani25@gmail.com>
2024-09-26 16:08:05 +0530

    Update Header

--- src/components/Header/Headerbar.jsx
+++ src/components/Header/Headerbar.jsx
@@ -13,10 +13,14 @@ const Headerbar = () => {
   const ref = useRef('');
   const navigate = useNavigate();
 
-  const handleClick = (event) => {
+  const handleMouseEnter = (event) => {
     setShow(!show);
     setTarget(event.target);
   };
+  const handleMouseLeave = (event) =>{
+    setShow(!show);
+    setTarget(event.target);
+  }
 
   const logout = async (event) => {
     event.preventDefault();
@@ -53,7 +57,7 @@ const Headerbar = () => {
             </svg>
           </Link>
           <div ref={ref}>
-            <Link onClick={handleClick}>
+            <Link onMouseEnter={handleMouseEnter}  onMouseLeave={handleMouseLeave}>
               <svg xmlns="http://www.w3.org/2000/svg" className="m-2 bi bi-people-fill" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
               </svg>
