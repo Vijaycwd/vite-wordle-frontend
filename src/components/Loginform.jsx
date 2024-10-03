69c0bc25d2b957444f28203a835b786fc89ce72d
Vijaycwd <vijisubramani25@gmail.com>
2024-10-03 14:02:24 +0530

    Update Loginform.jsx

--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -15,10 +15,15 @@ function Loginform() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
+    const [showLoginForm, setShowLoginForm] = useState(false);
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
     };
+
+    const showLoginformClick = () => {
+        setShowLoginForm(!showLoginForm);
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -58,59 +63,70 @@ function Loginform() {
                         <p className='text-center'>Please create your profile and then click the Wordle button and go from there!</p>
                         {!userAuthData || isEmptyObject ? (
                             <>
-                                <h4 className='mt-5'>Sign In</h4>
-                                <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                                    <Row className="mb-3">
-                                        <Col>
-                                            <Form.Group controlId="login-email">
-                                                <InputGroup hasValidation>
-                                                    <Form.Control
-                                                        type="email"
-                                                        placeholder="Email"
-                                                        required
-                                                        onChange={(e) => setEmail(e.target.value)}
-                                                        value={email}
-                                                        name="login-email"
-                                                    />
-                                                    <Form.Control.Feedback type="invalid">
-                                                        Please enter an email.
-                                                    </Form.Control.Feedback>
-                                                </InputGroup>
-                                            </Form.Group>
-                                        </Col>
-                                    </Row>
-                                    <Row className="mb-3">
-                                        <Col>
-                                            <Form.Group controlId="login-password">
-                                                <InputGroup>
-                                                    <Form.Control
-                                                        type={showPassword ? "text" : "password"}
-                                                        placeholder="Password"
-                                                        required
-                                                        onChange={(e) => setPassword(e.target.value)}
-                                                        value={password}
-                                                        name="login-password"
-                                                    />
-                                                    <InputGroup.Text>
-                                                        <i className={showPassword ? "fa fa-eye-slash" : "fa fa-eye"} onClick={togglePasswordVisibility}></i>
-                                                    </InputGroup.Text>
-                                                    <Form.Control.Feedback type="invalid">
-                                                        Please enter a password.
-                                                    </Form.Control.Feedback>
-                                                </InputGroup>
-                                            </Form.Group>
-                                        </Col>
-                                    </Row>
-                                    <Button type="submit">
-                                        <i className="fa fa-fw fa-sign-in-alt mr-1"></i>Sign In
-                                    </Button>
-                                </Form>
-                                <div className='btn-section my-3'>
-                                    <Link className="btn btn-sm btn-light d-block d-lg-inline-block mb-1 float-left" to="/register">New Account</Link>
-                                    <Link className="btn btn-sm btn-light d-block d-lg-inline-block mb-1 float-right" to="/reset-password">
-                                        <i className="fa fa-exclamation-triangle text-muted mr-1"></i> Forgot password
-                                    </Link>
+                                <div>
+                                    <Link className="btn btn-primary btn-lg mt-3" to="/register" style={{width:"60%"}}>Create Profile</Link>
                                 </div>
+                                {showLoginForm && (
+                                    <>
+                                        <div>
+                                        
+                                        <h4 className='mt-5'>Sign In</h4>
+                                        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                                            <Row className="mb-3">
+                                                <Col>
+                                                    <Form.Group controlId="login-email">
+                                                        <InputGroup hasValidation>
+                                                            <Form.Control
+                                                                type="email"
+                                                                placeholder="Email"
+                                                                required
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                                value={email}
+                                                                name="login-email"
+                                                            />
+                                                            <Form.Control.Feedback type="invalid">
+                                                                Please enter an email.
+                                                            </Form.Control.Feedback>
+                                                        </InputGroup>
+                                                    </Form.Group>
+                                                </Col>
+                                            </Row>
+                                            <Row className="mb-3">
+                                                <Col>
+                                                    <Form.Group controlId="login-password">
+                                                        <InputGroup>
+                                                            <Form.Control
+                                                                type={showPassword ? "text" : "password"}
+                                                                placeholder="Password"
+                                                                required
+                                                                onChange={(e) => setPassword(e.target.value)}
+                                                                value={password}
+                                                                name="login-password"
+                                                            />
+                                                            <InputGroup.Text>
+                                                                <i className={showPassword ? "fa fa-eye-slash" : "fa fa-eye"} onClick={togglePasswordVisibility}></i>
+                                                            </InputGroup.Text>
+                                                            <Form.Control.Feedback type="invalid">
+                                                                Please enter a password.
+                                                            </Form.Control.Feedback>
+                                                        </InputGroup>
+                                                    </Form.Group>
+                                                </Col>
+                                            </Row>
+                                            <Button type="submit">
+                                                <i className="fa fa-fw fa-sign-in-alt mr-1"></i>Sign In
+                                            </Button>
+                                        </Form>
+                                        <div className='btn-section my-3'>
+                                            <Link className="btn btn-sm btn-light d-block d-lg-inline-block mb-1 float-right" to="/reset-password">
+                                                <i className="fa fa-exclamation-triangle text-muted mr-1"></i> Forgot password
+                                            </Link>
+                                        </div>
+                                        </div>
+                                    </>
+                                )}
+                                {showLoginForm ? '' : <Button className="btn btn-primary btn-lg mt-3" onClick={showLoginformClick} style={{width:"60%"}}>Login
+                                </Button>}
                             </>
                         ) : (
                             <Link className="btn btn-primary btn-lg mt-3" to="/wordle">Wordle</Link>
