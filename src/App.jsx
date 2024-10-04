d7f92c7c0487503a53e81a82978b83bafadf5c02
Vijaycwd <vijisubramani25@gmail.com>
2024-10-04 14:33:44 +0530

    import home file

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,12 @@ import ProtectedRouter from './auth/Protect';
 import Dashboard from './pages/Dashboard';
 import NotFound from './pages/NotFound';
 import Statistics from './pages/Statistics';
+import Resetpwd from './auth/Resetpwd';
+import Resetpwdform from './auth/Resetpwdform';
 import Wordlestats from './components/Games/Wordle/Wordlestats';
 import UserProfile from './components/Games/Wordle/UserProfile';
 import Home from './pages/Home';
+
 
 function App() {
   return (
@@ -22,6 +25,8 @@ function App() {
         <Route  path="/register" element={<Registerform />} />
         <Route  path="*" element={<NotFound/>} />
         <Route  path='/wordle' element={<Statistics/>}/>
+        <Route exact path="/reset-password" element={<Resetpwd />} />
+        <Route exact path="/reset-password/:id/:token" element={<Resetpwdform />} />
         <Route  path='/' element={<ProtectedRouter/>}>
           <Route  path='/dashboard' element={<Dashboard/>}/>
           <Route  path= '/wordlestats' element={<Wordlestats/>}/>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';  // Make sure this is imported
+
+function Home() {
+    const USER_AUTH_DATA = JSON.parse(localStorage.getItem('auth'));
+    const userAuthData = USER_AUTH_DATA;
+    const navigate = useNavigate();
+    const loginformClick = () => {
+        navigate('/login');
+    }
+    const isEmptyObject = userAuthData && Object.keys(userAuthData).length === 0;
+
+    return (
+        <>
+            <Container className="login-section">
+                <Row className="align-content-center justify-content-center">
+                    <Col md={4} className='bg-white px-5 py-3 text-center'>
+                        <p className='fs-4 text-center'>Welcome to <b>WordGAMLE!</b></p>
+                        <p className='text-center'>Soon we’ll be playing various games and creating groups and leaderboards.  But, for now we’re just playing Wordle and storing results.
+                        </p>
+                        <Link className="btn btn-primary btn-lg my-3" to="/wordle">Wordle</Link>
+                        <p className='text-center'>Please create your profile and then click the Wordle button and go from there!</p>
+                        {!userAuthData || isEmptyObject ? (
+                            <>
+                                <div>
+                                    <Link className="btn btn-primary btn-lg my-3" to="/register" style={{width:"60%"}}>Create Profile</Link>
+                                    <Button className="btn btn-primary btn-lg mt-3" onClick={loginformClick} style={{width:"60%"}}>Login
+                                        </Button>
+                                </div>
+                            </>
+                        ) : (
+                            <div></div>
+                        )}
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+}
+
+export default Home;
