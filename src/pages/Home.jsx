51b9fe8a3658113207a873ffbc9eda947c072e88
Vijaycwd <vijisubramani25@gmail.com>
2024-11-05 15:09:42 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,14 +18,18 @@ function Home() {
             <Container className="login-section">
                 <Row className="align-content-center justify-content-center">
                     <Col md={4} className='bg-white px-3 py-3 text-center'>
-                        <p className='fs-4 text-center'>Welcome to <b>WordGAMLE!</b></p>
-                        <p className='text-center'>Eventually we’ll add more games and will be creating groups and leaderboards.  But, for now we’re just playing Wordle, Connections and Phrazle and storing results.
-                        </p>
-                        <div className="d-flex justify-content-around">
-                            <Link className="btn btn-primary btn-lg my-3" to="/wordle">Wordle</Link>
-                            <Link className="btn btn-primary btn-lg my-3" to="/connections">Connections</Link>
-                            <Link className="btn btn-primary btn-lg my-3" to="/phrazle">Phrazle</Link>
-                        </div>
+                        <Row>
+                            <Col>
+                            <p className='fs-4 text-center'>Welcome to <b>WordGAMLE!</b></p>
+                            <p className='text-center'>Eventually we’ll add more games and will be creating groups and leaderboards.  But, for now we’re just playing Wordle, Connections and Phrazle and storing results.
+                            </p>
+                            </Col>
+                        </Row>
+                        <Row className='py-4'>
+                            <Col className='py-2 px-0' md={4}><Link className="btn btn-primary btn-lg" to="/wordle">Wordle</Link></Col>
+                            <Col className='py-2 px-0' md={4}><Link className="btn btn-primary btn-lg" to="/connections">Connections</Link></Col>
+                            <Col className='py-2 px-0' md={4}><Link className="btn btn-primary btn-lg" to="/phrazle">Phrazle</Link></Col>
+                        </Row>
                         {!userAuthData || isEmptyObject ? (
                             <>
                                 <div>
